8b1f016e9cefec474c2277ee58fe4638fd0c5ff5
Aster Seker <git.electroyar@gmail.com>
2025-08-27 03:24:35 +0300

    refactor(config): centralize default file paths

--- include/imguix/config/fonts.hpp
+++ include/imguix/config/fonts.hpp
@@ -10,4 +10,16 @@
 #   define IMGUIX_FONTS_CONFIG u8"data/resources/fonts/fonts.json"
 #endif
 
+#ifndef IMGUIX_FONTS_CONFIG_BASENAME
+#   define IMGUIX_FONTS_CONFIG_BASENAME u8"fonts.json"
+#endif
+
+#ifndef IMGUIX_FONTS_FALLBACK_BODY_BASENAME
+#   define IMGUIX_FONTS_FALLBACK_BODY_BASENAME u8"Roboto-Medium.ttf"
+#endif
+
+#ifndef IMGUIX_FONTS_FALLBACK_ICONS_BASENAME
+#   define IMGUIX_FONTS_FALLBACK_ICONS_BASENAME u8"forkawesome-webfont.ttf"
+#endif
+
 #endif // _IMGUIX_CONFIG_FONTS_HPP_INCLUDED
