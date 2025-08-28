58d536920910d033855ddf3b2d07a8cfe22ce5b6
Aster Seker <git.electroyar@gmail.com>
2025-08-28 05:21:01 +0300

    fix(utils): centralize dependent false

---
+++ include/imguix/utils/detail/common.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef _IMGUIX_UTILS_DETAIL_COMMON_HPP_INCLUDED
+#define _IMGUIX_UTILS_DETAIL_COMMON_HPP_INCLUDED
+
+namespace ImGuiX::Utils::detail {
+
+    template<class T>
+    inline constexpr bool dependent_false_v = false;
+
+} // namespace ImGuiX::Utils::detail
+
+#endif // _IMGUIX_UTILS_DETAIL_COMMON_HPP_INCLUDED
