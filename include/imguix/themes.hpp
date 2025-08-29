ee8d887ba521d269c01d5c84e70b27b85cd08a82
Aster Seker <git.electroyar@gmail.com>
2025-08-29 05:17:14 +0300

    feat(themes): add aggregated theme include

---
+++ include/imguix/themes.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef _IMGUIX_THEMES_HPP_INCLUDED
+#define _IMGUIX_THEMES_HPP_INCLUDED
+
+/// \\file themes.hpp
+/// \\brief Aggregated include for all built-in ImGuiX themes.
+
+#include <imguix/themes/CorporateGreyTheme.hpp>
+#include <imguix/themes/DarkCharcoalTheme.hpp>
+#include <imguix/themes/DarkGraphiteTheme.hpp>
+#include <imguix/themes/DarkTealTheme.hpp>
+#include <imguix/themes/DeepDarkTheme.hpp>
+#include <imguix/themes/GoldBlackTheme.hpp>
+#include <imguix/themes/GreenBlueTheme.hpp>
+#include <imguix/themes/LightGreenTheme.hpp>
+#include <imguix/themes/OSXTheme.hpp>
+#include <imguix/themes/PearlLightTheme.hpp>
+#include <imguix/themes/SlateDarkTheme.hpp>
+#include <imguix/themes/VisualStudioDarkTheme.hpp>
+
+#endif // _IMGUIX_THEMES_HPP_INCLUDED
