f0d69ce227f96a18c81231a82b68cf0dd8e8377f
Yaroslav <elektroyar@yandex.ru>
2025-09-01 20:59:35 +0300

    feat(imcmd): integrate imgui-command-palette (command palette UI)

---
+++ cmake/deps/imcmd.cmake
@@ -0,0 +1,28 @@
+# Provides: imguix_use_or_fetch_imcmd(<OUT_VAR>)
+# Exports target: imcmd::imcmd
+function(imguix_use_or_fetch_imcmd OUT_VAR)
+    set(_DIR "${IMCMD_DIR}")
+    if(NOT _DIR)
+        set(_DIR "${PROJECT_SOURCE_DIR}/libs/imgui-command-palette")
+    endif()
+    if(NOT EXISTS "${_DIR}/imcmd_command_palette.cpp")
+        message(FATAL_ERROR "imgui-command-palette not found at ${_DIR}. "
+                            "Set IMCMD_DIR or add submodule libs/imgui-command-palette.")
+    endif()
+
+    add_library(imcmd STATIC
+        "${_DIR}/imcmd_command_palette.cpp"
+        "${_DIR}/imcmd_fuzzy_search.cpp"
+    )
+    add_library(imcmd::imcmd ALIAS imcmd)
+
+    target_include_directories(imcmd PUBLIC "${_DIR}")
+    target_link_libraries(imcmd PUBLIC imgui::imgui)
+
+    set_target_properties(imcmd PROPERTIES
+        CXX_STANDARD 17
+        POSITION_INDEPENDENT_CODE ON
+        PUBLIC_HEADER "${_DIR}/imcmd_command_palette.h;${_DIR}/imcmd_fuzzy_search.h"
+    )
+    set(${OUT_VAR} imcmd::imcmd PARENT_SCOPE)
+endfunction()
--- include/imguix/widgets/external_widgets_demo.hpp
+++ include/imguix/widgets/external_widgets_demo.hpp
@@ -37,197 +37,261 @@
 #include <ImGuiColorTextEdit.h>
 #endif
 
-namespace ImGuiX {
-namespace Widgets {
+namespace ImGuiX::Widgets {
 
-#ifdef IMGUI_ENABLE_IMNODEFLOW
-class SimpleSum final : public ImFlow::BaseNode {
-public:
-    SimpleSum() {
-        setTitle("Simple sum");
-        setStyle(ImFlow::NodeStyle::green());
-        addIN<int>("A", 0, ImFlow::ConnectionFilter::SameType());
-        addOUT<int>("Result", nullptr)
-            ->behaviour([this]() { return getInVal<int>("A") + b_; });
+#   ifdef IMGUI_ENABLE_IMNODEFLOW
+    class SimpleSum final : public ImFlow::BaseNode {
+    public:
+        SimpleSum() {
+            setTitle("Simple sum");
+            setStyle(ImFlow::NodeStyle::green());
+            addIN<int>("A", 0, ImFlow::ConnectionFilter::SameType());
+            addOUT<int>("Result", nullptr)
+                ->behaviour([this]() { return getInVal<int>("A") + b_; });
+        }
+        void draw() override {
+            ImGui::SetNextItemWidth(100.0f);
+            ImGui::InputInt("B", &b_);
+        }
+    private:
+        int b_ = 0;
+    };
+
+    static ImFlow::ImNodeFlow g_editor;
+
+    inline void DrawNodeEditorFrame() {
+        static bool inited = false;
+        if (!inited) {
+            g_editor.placeNodeAt<SimpleSum>(ImVec2(120, 80));
+            inited = true;
+        }
+        ImGui::Begin("Node Editor");
+        g_editor.update();
+        ImGui::End();
     }
-    void draw() override {
-        ImGui::SetNextItemWidth(100.0f);
-        ImGui::InputInt("B", &b_);
+#   endif
+
+#   ifdef IMGUI_ENABLE_IMCMD
+namespace {
+    inline bool show_cmd_palette = false;
+    // Ensure context exists (CreateContext also binds it as current if none)
+    inline ImCmd::Context* ImCmdCtx() {
+        ImCmd::Context* ctx = ImCmd::GetCurrentContext();
+        if (!ctx) ctx = ImCmd::CreateContext();
+        return ctx;
     }
-private:
-    int b_ = 0;
-};
 
-static ImFlow::ImNodeFlow g_editor;
-
-inline void DrawNodeEditorFrame() {
-    static bool inited = false;
-    if (!inited) {
-        g_editor.placeNodeAt<SimpleSum>(ImVec2(120, 80));
+    // Register commands once
+    inline void ImCmdRegisterCommands() {
+        static bool inited = false;
+        if (inited) return;
         inited = true;
+
+        (void)ImCmdCtx(); // make sure context is ready
+
+        ImCmd::AddCommand(ImCmd::Command{
+            "Open Settings",
+            [] { /* open your settings panel */ },
+            {}, {}, // SubsequentCallback / TerminatingCallback not used here
+        });
+
+        ImCmd::AddCommand(ImCmd::Command{
+            "Toggle Theme",
+            [] { /* switch theme */ },
+            {}, {},
+        });
+
+        ImCmd::AddCommand(ImCmd::Command{
+            "Help: About",
+            [] { /* show about modal */ },
+            {}, {},
+        });
+
+        // Example with options via Prompt + SubsequentCallback
+        ImCmd::AddCommand(ImCmd::Command{
+            "Switch Layout",
+            [] {
+                ImCmd::Prompt({"Classic", "Dark", "Cyber-Y2K"}); // shows options
+            },
+            [](int idx) {
+                // idx is 0..N-1 depending on selection
+                // apply layout by idx
+            },
+            {}, // optional TerminatingCallback
+        });
     }
-    ImGui::Begin("Node Editor");
-    g_editor.update();
-    ImGui::End();
-}
-#endif
+} // namespace
 
-inline void DemoExternalWidgets() {
-    static bool show_imgui_demo = false;
-#ifdef IMGUI_ENABLE_IMPLOT
-    static bool show_implot_demo = false;
-#endif
-#ifdef IMGUI_ENABLE_IMPLOT3D
-    static bool show_implot3d_demo = false;
-#endif
-#ifdef IMGUI_ENABLE_IMNODEFLOW
-    static bool show_imnodeflow_demo = false;
-#endif
-#ifdef IMGUI_ENABLE_IMGUIFILEDIALOG
-    static bool show_igfd = false;
-#endif
-#ifdef IMGUI_ENABLE_PFD
-    static bool show_pfd = false;
-#endif
-#ifdef IMGUI_ENABLE_IMSPINNER
-    static bool show_imspinner_demo = false;
-#endif
-#ifdef IMGUI_ENABLE_IMTEXTEDITOR
-    static bool show_text_editor = false;
-#endif
+    inline void DrawImCmdDemo() {
+        ImCmdRegisterCommands();
 
-#ifdef IMGUI_ENABLE_IMCMD
-    static bool show_cmd_palette = false;
-#endif
+        // VSCode-like hotkey: Ctrl+Shift+P
+        if (ImGui::IsKeyChordPressed(ImGuiMod_Ctrl | ImGuiMod_Shift | ImGuiKey_P))
+            show_cmd_palette = true;
 
-    if (ImGui::BeginMainMenuBar()) {
-        if (ImGui::BeginMenu("Demo")) {
-            if (ImGui::MenuItem("ImGui", nullptr, false, !show_imgui_demo)) show_imgui_demo = true;
-#ifdef IMGUI_ENABLE_IMPLOT
-            if (ImGui::MenuItem("ImPlot", nullptr, false, !show_implot_demo)) show_implot_demo = true;
-#endif
-#ifdef IMGUI_ENABLE_IMPLOT3D
-            if (ImGui::MenuItem("ImPlot3D", nullptr, false, !show_implot3d_demo)) show_implot3d_demo = true;
-#endif
-#ifdef IMGUI_ENABLE_IMNODEFLOW
-            if (ImGui::MenuItem("ImNodeFlow", nullptr, false, !show_imnodeflow_demo)) show_imnodeflow_demo = true;
-#endif
-#ifdef IMGUI_ENABLE_IMGUIFILEDIALOG
-            if (ImGui::MenuItem("File Dialog", nullptr, false, !show_igfd)) show_igfd = true;
-#endif
-#ifdef IMGUI_ENABLE_PFD
-            if (ImGui::MenuItem("File Dialog (pfd)", nullptr, false, !show_pfd)) show_pfd = true;
-#endif
-#ifdef IMGUI_ENABLE_IMCMD
-            if (ImGui::MenuItem("Command Palette", "Ctrl+Shift+P", false, !show_cmd_palette)) show_cmd_palette = true;
-#endif
-#ifdef IMGUI_ENABLE_IMSPINNER
-            if (ImGui::MenuItem("ImSpinner", nullptr, false, !show_imspinner_demo)) show_imspinner_demo = true;
-#endif
-#ifdef IMGUI_ENABLE_IMTEXTEDITOR
-            if (ImGui::MenuItem("Code Editor", nullptr, false, !show_text_editor)) show_text_editor = true;
-#endif
-            ImGui::EndMenu();
+        if (show_cmd_palette) {
+            // Window helper with close toggle
+            ImCmd::CommandPaletteWindow("Command Palette", &show_cmd_palette);
+            // Or bare widget:
+            // ImCmd::CommandPalette("Command Palette");
         }
-        ImGui::EndMainMenuBar();
     }
+#   endif
 
-    if (show_imgui_demo) ImGui::ShowDemoWindow(&show_imgui_demo);
-#ifdef IMGUI_ENABLE_IMPLOT
-    if (show_implot_demo) ImPlot::ShowDemoWindow(&show_implot_demo);
-#endif
-#ifdef IMGUI_ENABLE_IMPLOT3D
-    if (show_implot3d_demo) ImPlot3D::ShowDemoWindow(&show_implot3d_demo);
-#endif
-#ifdef IMGUI_ENABLE_IMNODEFLOW
-    if (show_imnodeflow_demo) {
-        ImGui::Begin("ImNodeFlow Demo", &show_imnodeflow_demo);
-        DrawNodeEditorFrame();
-        ImGui::End();
-    }
-#endif
-#ifdef IMGUI_ENABLE_IMGUIFILEDIALOG
-    if (show_igfd) {
-        ImGui::Begin("ImGuiFileDialog", &show_igfd);
-        if (ImGui::Button("Open...")) {
-            IGFD::FileDialogConfig cfg;
-            cfg.path = ".";
-            ImGuiFileDialog::Instance()->OpenDialog("OpenDlg", "Open File", ".*", cfg);
-        }
-        if (ImGuiFileDialog::Instance()->Display("OpenDlg")) {
-            if (ImGuiFileDialog::Instance()->IsOk()) {
-                const std::string path = ImGuiFileDialog::Instance()->GetFilePathName();
-                (void)path;
+	/// \berif
+    inline void DemoExternalWidgets() {
+        static bool show_imgui_demo = false;
+#   ifdef IMGUI_ENABLE_IMPLOT
+        static bool show_implot_demo = false;
+#   endif
+#   ifdef IMGUI_ENABLE_IMPLOT3D
+        static bool show_implot3d_demo = false;
+#   endif
+#   ifdef IMGUI_ENABLE_IMNODEFLOW
+        static bool show_imnodeflow_demo = false;
+#   endif
+#   ifdef IMGUI_ENABLE_IMGUIFILEDIALOG
+        static bool show_igfd = false;
+#   endif
+#   ifdef IMGUI_ENABLE_PFD
+        static bool show_pfd = false;
+#   endif
+#   ifdef IMGUI_ENABLE_IMSPINNER
+        static bool show_imspinner_demo = false;
+#   endif
+#   ifdef IMGUI_ENABLE_IMTEXTEDITOR
+        static bool show_text_editor = false;
+#   endif
+
+        if (ImGui::BeginMainMenuBar()) {
+            if (ImGui::BeginMenu("Demo")) {
+                if (ImGui::MenuItem("ImGui", nullptr, false, !show_imgui_demo)) show_imgui_demo = true;
+#   ifdef IMGUI_ENABLE_IMPLOT
+                if (ImGui::MenuItem("ImPlot", nullptr, false, !show_implot_demo)) show_implot_demo = true;
+#   endif
+#   ifdef IMGUI_ENABLE_IMPLOT3D
+                if (ImGui::MenuItem("ImPlot3D", nullptr, false, !show_implot3d_demo)) show_implot3d_demo = true;
+#   endif
+#   ifdef IMGUI_ENABLE_IMNODEFLOW
+                if (ImGui::MenuItem("ImNodeFlow", nullptr, false, !show_imnodeflow_demo)) show_imnodeflow_demo = true;
+#   endif
+#   ifdef IMGUI_ENABLE_IMGUIFILEDIALOG
+                if (ImGui::MenuItem("File Dialog", nullptr, false, !show_igfd)) show_igfd = true;
+#   endif
+#   ifdef IMGUI_ENABLE_PFD
+                if (ImGui::MenuItem("File Dialog (pfd)", nullptr, false, !show_pfd)) show_pfd = true;
+#   endif
+#   ifdef IMGUI_ENABLE_IMCMD
+                if (ImGui::MenuItem("Command Palette", "Ctrl+Shift+P", false, !show_cmd_palette)) show_cmd_palette = true;
+#   endif
+#   ifdef IMGUI_ENABLE_IMSPINNER
+                if (ImGui::MenuItem("ImSpinner", nullptr, false, !show_imspinner_demo)) show_imspinner_demo = true;
+#   endif
+#   ifdef IMGUI_ENABLE_IMTEXTEDITOR
+                if (ImGui::MenuItem("Code Editor", nullptr, false, !show_text_editor)) show_text_editor = true;
+#   endif
+                ImGui::EndMenu();
             }
-            ImGuiFileDialog::Instance()->Close();
+            ImGui::EndMainMenuBar();
         }
-        ImGui::End();
-    }
-#endif
-#ifdef IMGUI_ENABLE_PFD
-    if (show_pfd) {
-        ImGui::Begin("portable-file-dialogs", &show_pfd);
-        static std::string last_open, last_save;
-        if (ImGui::Button("Open...")) {
-            auto sel = pfd::open_file("Open file",
-                                      ".",
-                                      { "All files", "*" },
-                                      pfd::opt::multiselect).result();
-            if (!sel.empty()) last_open = sel.front();
-        }
-        ImGui::SameLine();
-        ImGui::TextDisabled("%s", last_open.c_str());
-        if (ImGui::Button("Save As...")) {
-            last_save = pfd::save_file("Save file", ".", { "Text", "*.txt", "All files", "*" }).result();
-        }
-        ImGui::SameLine();
-        ImGui::TextDisabled("%s", last_save.c_str());
-        if (ImGui::Button("Message box")) {
-            pfd::message("Notice", "Operation finished.", pfd::choice::ok, pfd::icon::info).result();
-        }
-        ImGui::End();
-    }
-#endif
-#ifdef IMGUI_ENABLE_IMSPINNER
-    if (show_imspinner_demo) {
-        ImGui::Begin("ImSpinner Demo", &show_imspinner_demo);
-#ifdef IMSPINNER_DEMO
-        ImSpinner::demoSpinners();
-#else
-        ImGui::TextDisabled("Build without IMSPINNER_DEMO");
-#endif
-        ImGui::End();
-    }
-#endif
-#ifdef IMGUI_ENABLE_IMTEXTEDITOR
-    static ImTextEdit::TextEditor s_editor;
-    if (show_text_editor) {
-        static bool init = false;
-        if (!init) {
-            s_editor.SetLanguageDefinition(ImTextEdit::CPlusPlus());
-            s_editor.SetPalette(ImTextEdit::GetDarkPalette());
-            s_editor.SetShowWhitespaces(false);
-            s_editor.SetText("// Hello from ImGuiColorTextEdit\nint main(){return 0;}\n");
-            init = true;
-        }
-        ImGui::Begin("Code Editor", &show_text_editor);
-        s_editor.Render("TextEditor");
-        ImGui::End();
-    }
-#endif
-#ifdef IMGUI_ENABLE_IMCMD
-    if (show_cmd_palette) {
-        // 1) Создай/держи состояние палитры (объект/синглтон) где-то вне кадра
-        // 2) Зарегистрируй команды (один раз)
-        // 3) Вызови функцию отрисовки палитры внутри окна/поверх всего
-        //    (см. examples/ в репо — там готовая схема регистрации и отображения)
-        // Примерно: palette.OpenOnShortcut(Ctrl+Shift+P); palette.Render(...);
-    }
-#endif
-}
 
-} // namespace Widgets
-} // namespace ImGuiX
+        if (show_imgui_demo) ImGui::ShowDemoWindow(&show_imgui_demo);
+
+#   ifdef IMGUI_ENABLE_IMPLOT
+        if (show_implot_demo) ImPlot::ShowDemoWindow(&show_implot_demo);
+#   endif
+
+#   ifdef IMGUI_ENABLE_IMPLOT3D
+        if (show_implot3d_demo) ImPlot3D::ShowDemoWindow(&show_implot3d_demo);
+#   endif
+
+#   ifdef IMGUI_ENABLE_IMNODEFLOW
+        if (show_imnodeflow_demo) {
+            ImGui::Begin("ImNodeFlow Demo", &show_imnodeflow_demo);
+            DrawNodeEditorFrame();
+            ImGui::End();
+        }
+#   endif
+
+#   ifdef IMGUI_ENABLE_IMGUIFILEDIALOG
+        if (show_igfd) {
+            ImGui::Begin("ImGuiFileDialog", &show_igfd);
+            if (ImGui::Button("Open...")) {
+                IGFD::FileDialogConfig cfg;
+                cfg.path = ".";
+                ImGuiFileDialog::Instance()->OpenDialog("OpenDlg", "Open File", ".*", cfg);
+            }
+            if (ImGuiFileDialog::Instance()->Display("OpenDlg")) {
+                if (ImGuiFileDialog::Instance()->IsOk()) {
+                    const std::string path = ImGuiFileDialog::Instance()->GetFilePathName();
+                    (void)path;
+                }
+                ImGuiFileDialog::Instance()->Close();
+            }
+            ImGui::End();
+        }
+#   endif
+
+#   ifdef IMGUI_ENABLE_PFD
+        if (show_pfd) {
+            ImGui::Begin("portable-file-dialogs", &show_pfd);
+            static std::string last_open, last_save;
+            if (ImGui::Button("Open...")) {
+                auto sel = pfd::open_file("Open file",
+                                          ".",
+                                          { "All files", "*" },
+                                          pfd::opt::multiselect).result();
+                if (!sel.empty()) last_open = sel.front();
+            }
+            ImGui::SameLine();
+            ImGui::TextDisabled("%s", last_open.c_str());
+            if (ImGui::Button("Save As...")) {
+                last_save = pfd::save_file("Save file", ".", { "Text", "*.txt", "All files", "*" }).result();
+            }
+            ImGui::SameLine();
+            ImGui::TextDisabled("%s", last_save.c_str());
+            if (ImGui::Button("Message box")) {
+                pfd::message("Notice", "Operation finished.", pfd::choice::ok, pfd::icon::info).result();
+            }
+            ImGui::End();
+        }
+#   endif
+
+#   ifdef IMGUI_ENABLE_IMSPINNER
+        if (show_imspinner_demo) {
+            ImGui::Begin("ImSpinner Demo", &show_imspinner_demo);
+#   ifdef IMSPINNER_DEMO
+            ImSpinner::demoSpinners();
+#   else
+            ImGui::TextDisabled("Build without IMSPINNER_DEMO");
+#   endif
+            ImGui::End();
+        }
+#   endif
+
+#   ifdef IMGUI_ENABLE_IMTEXTEDITOR
+        static ImTextEdit::TextEditor s_editor;
+        if (show_text_editor) {
+            static bool init = false;
+            if (!init) {
+                s_editor.SetLanguageDefinition(ImTextEdit::CPlusPlus());
+                s_editor.SetPalette(ImTextEdit::GetDarkPalette());
+                s_editor.SetShowWhitespaces(false);
+                s_editor.SetText("// Hello from ImGuiColorTextEdit\nint main(){return 0;}\n");
+                init = true;
+            }
+            ImGui::Begin("Code Editor", &show_text_editor);
+            s_editor.Render("TextEditor");
+            ImGui::End();
+        }
+#   endif
+
+#   ifdef IMGUI_ENABLE_IMCMD
+        DrawImCmdDemo();
+#   endif
+    } // DemoExternalWidgets()
+
+} // namespace ImGuiX::Widgets
 
 #endif // IMGUIX_DEMO
 
