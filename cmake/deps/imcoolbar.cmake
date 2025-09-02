e9617e420850b732719995d7c3d6f6a313545397
Yaroslav <elektroyar@yandex.ru>
2025-09-02 21:26:09 +0300

    feat(imcoolbar): integrate ImCoolBar and add demo

---
+++ cmake/deps/imcoolbar.cmake
@@ -0,0 +1,27 @@
+# Provides: imguix_use_or_fetch_imcoolbar(<OUT_VAR>)
+# Exports target: imcoolbar::imcoolbar
+function(imguix_use_or_fetch_imcoolbar OUT_VAR)
+    set(_DIR "${IMCOOLBAR_DIR}")
+    if(NOT _DIR)
+        set(_DIR "${PROJECT_SOURCE_DIR}/libs/ImCoolBar")
+    endif()
+
+    if(NOT EXISTS "${_DIR}/ImCoolBar.cpp")
+        message(FATAL_ERROR "ImCoolBar not found at ${_DIR}. "
+                            "Set IMCOOLBAR_DIR or add submodule libs/ImCoolBar.")
+    endif()
+
+    add_library(imcoolbar STATIC "${_DIR}/ImCoolBar.cpp")
+    add_library(imcoolbar::imcoolbar ALIAS imcoolbar)
+
+    target_include_directories(imcoolbar PUBLIC "${_DIR}")
+    target_link_libraries(imcoolbar PUBLIC imgui::imgui)
+
+    set_target_properties(imcoolbar PROPERTIES
+        CXX_STANDARD 17
+        POSITION_INDEPENDENT_CODE ON
+        PUBLIC_HEADER "${_DIR}/ImCoolBar.h"
+    )
+
+    set(${OUT_VAR} imcoolbar::imcoolbar PARENT_SCOPE)
+endfunction()
